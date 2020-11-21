d2c6afe36c6cb697426e6f83fe53dfe5d56e5936
AhaanSeth <72495179+AhaanSeth@users.noreply.github.com>
2020-11-22 02:04:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+//Create variables here
+var dog, happyDog, database, foodS, foodStock;
+var dogImage1, dogImage2;
+
+function preload()
+{
+  //load images here
+  dogImage1 = loadImage("images/dogImg.png");
+  dogImage2 = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+	createCanvas(500, 500);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
